2fb5532d14b9935de1f062aafbdd24b3a18103e3
ganesh <ganeshredcobra@gmail.com>
2016-11-28 23:45:45 +0530

    List view modified

--- ListWidget/mainwindow.cpp
+++ ListWidget/mainwindow.cpp
@@ -6,9 +6,24 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // populate the items of the list
+    for(int i = 0; i < 10; i++)
+    {
+        ui->listWidget->addItem("Item " + QString::number(i));
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    // Get the pointer to the currently selected item.
+        QListWidgetItem *item = ui->listWidget->currentItem();
+
+        // Set the text color and its background color using the pointer to the item.
+        item->setTextColor(Qt::white);
+        item->setBackgroundColor(Qt::blue);
 }
